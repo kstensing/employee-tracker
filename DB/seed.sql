5637e9d6437f19621ed7681ea8c115b4efd74248
kstensing <stensingk@gmail.com>
2022-03-11 20:17:09 -0600

    added seeds

---
+++ DB/seed.sql
@@ -0,0 +1,39 @@
+INSERT INTO department 
+    (name)
+VALUES
+('Engineering'),
+('Legal'),
+('Finance'),
+('Sales');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+('Lead Engineer', 150000, 1),
+('Software Engineer', 120000, 1),
+('Account Manager', 160000, 3),
+('Legal Team Lead', 250000, 2),
+('Lawyer' 190000, 2),
+('Accountant', 125000, 3),
+('Salesperson', 80000, 4),
+('Sales Lead', 100000, 4);
+
+INSERT INTO employee
+    (first_name, last_name)
+VALUES
+('Taylor', 'Swift'),
+('Elvis', 'Presley'),
+('Jimi', 'Hendrix'),
+('Jay', 'Z'),
+('Louis', 'Armstrong'),
+('Wolfgang', 'Mozart'),
+('Ray', 'Charles'),
+('Whitney', 'Houston'),
+('Aretha', 'Franklin'),
+('Dave', 'Grohl'),
+('Paul', 'McCartney'),
+('Phil', 'Collins'),
+('Frank', 'Sinatra'),
+('Lady', 'Gaga');
+
+
