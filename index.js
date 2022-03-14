04ab8c6926c5489907622032daf392b49e269fb7
kstensing <stensingk@gmail.com>
2022-03-13 22:42:23 -0500

    added fuction to add employee

--- index.js
+++ index.js
@@ -64,8 +64,7 @@ const viewAllDepts = () => {
 const viewAllRoles = () => {
     const sql = `SELECT roles.id, roles.title, roles.salary, departments.dept_name 
                 FROM roles
-                LEFT JOIN departments ON roles.department_id=departments.id`
-                ;
+                LEFT JOIN departments ON roles.department_id=departments.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             console.error(err);
@@ -79,9 +78,10 @@ const viewAllRoles = () => {
 const viewAllEmployees = () => {
     const sql = `SELECT employees.id, employees.first_name, employees.last_name, concat(m.first_name," ",m.last_name) AS "manager name", roles.title, roles.salary, departments.dept_name AS "department"
                 FROM roles 
-                JOIN employees ON roles.id=employees.id
+                JOIN employees ON roles.id=employees.role_id
                 JOIN departments ON roles.department_id=departments.id
                 LEFT JOIN employees m ON employees.manager_id=m.id
+                ORDER BY employees.id
                 `;
     db.query(sql, (err, rows) => {
         if (err) {
@@ -116,21 +116,22 @@ const addDepartment = () => {
 const addRole = () => {
     return inquirer
         .prompt([{
-            type: 'input',
-            name: 'roleTitle',
-            message: "What is the title of the new role?",
-        },
-        {
-            type: 'input',
-            name: 'roleSalary',
-            message: "What is the salary of the new role?",
-        },
-        {
-            type: 'input',
-            name: 'roleDept',
-            message: "Which department is the new role in?",
-        }, ])
-        .then(data => {         
+                type: 'input',
+                name: 'roleTitle',
+                message: "What is the title of the new role?",
+            },
+            {
+                type: 'input',
+                name: 'roleSalary',
+                message: "What is the salary of the new role?",
+            },
+            {
+                type: 'input',
+                name: 'roleDept',
+                message: "Which department is the new role in?",
+            },
+        ])
+        .then(data => {
             const sql = `SELECT * FROM departments WHERE dept_name = ?`;
             db.query(sql, data.roleDept, (err, rows) => {
                 if (err) {
@@ -147,9 +148,70 @@ const addRole = () => {
                     start();
                 });
             });
+        })
+};
 
+const addEmployee = () => {
+    return inquirer
+        .prompt([{
+                type: 'input',
+                name: 'firstName',
+                message: "What is the employee's first name?",
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: "What is the employee's last name?",
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: "What is the employee's role?",
+            },
+            {
+                type: 'input',
+                name: 'managerFirstName',
+                message: "What is the manager's first name?",
+            },
+            {
+                type: 'input',
+                name: 'managerLastName',
+                message: "What is the manager's last name?",
+            },
+        ])
+        .then(data => {
+            const roleSql = `SELECT * FROM roles WHERE title = ?`;
+            console.log(data.role);
+            db.query(roleSql, data.role, (err, rows) => {
+                console.log(rows)
+                if (err) {
+                    console.error(err);
+                    console.log("That role doesn't exist! Please add the role first, then add the employee.")
+                    return;
+                }
+                const managerSql = `SELECT * FROM employees WHERE first_name=? and last_name=?`;
+                db.query(managerSql, [data.managerFirstName, data.managerLastName], (err, manager) => {
+                    if (err) {
+                        console.error(err);
+                        console.log("That manager doesn't exist! Please add the manager as an employee and try again.")
+                        return;
+                    }
+                    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+                    console.log(rows[0].id);
+                    console.log(manager[0].id);
+                    db.query(sql, [data.firstName, data.lastName, rows[0].id, manager[0].id], (err, rows) => {
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                        start();
+                    });
+                });
+            });
 
         })
 };
+
+
 
 start();
